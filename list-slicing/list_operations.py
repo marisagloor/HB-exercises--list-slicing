985ad60f0123fbe879de15af612b5436ce344df9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-11 12:27:40 -0700

    inner_four_end and replace_head

--- list-slicing/list_operations.py
+++ list-slicing/list_operations.py
@@ -138,7 +138,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -152,8 +152,8 @@ def replace_head(input_list):
     True
 
     """
-
-    pass
+    input_list[0] = 42
+    
 
 
 def replace_third_and_last(input_list):
