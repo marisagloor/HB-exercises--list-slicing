2f5f2d9d4060e0345705fed563a1269a9e84313c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-11 12:09:24 -0700

    first_three and last_five

--- list-slicing/list_operations.py
+++ list-slicing/list_operations.py
@@ -83,7 +83,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -96,7 +96,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5: ]
 
 
 def middle(input_list):
